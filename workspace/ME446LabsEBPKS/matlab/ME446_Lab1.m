d5acd1d750ea654659d79325d0b3e8b3bd592183
Shah <pks11@illinois.edu>
2024-02-02 11:57:42 -0600

    Lab1 2nd Feb

---
+++ workspace/ME446LabsEBPKS/matlab/ME446_Lab1.m
@@ -0,0 +1,56 @@
+%pks11 %elliana2
+%ME446
+
+%Finding Homogeneous transformation matrix syymobilically
+
+clear all;
+
+
+L1 = 0.254;
+L2 = 0.254;
+L3 = 0.254;
+
+syms theta1 theta2 theta3;
+
+
+
+h01 = [[cos(theta1) 0 -sin(theta1) 0];
+    [sin(theta1) 0 cos(theta1) 0];
+    [0 -1 0 0.254];
+    [0 0 0 1]];
+
+h12 = [[cos(theta2) -sin(theta2) 0 0.254*cos(theta2)];
+    [sin(theta2) cos(theta2) 0 0.254*sin(theta2)];
+    [0 0 1 0];
+    [0 0 0 1]];
+
+h23 = [[cos(theta3) -sin(theta3) 0 0.254*cos(theta3)];
+    [sin(theta3) cos(theta3) 0 0.254*sin(theta3)];
+    [0 0 1 0];
+    [0 0 0 1]];
+
+h03 = h01*h12*h23;
+
+syms theta1m theta2m theta3m;
+theta1 = theta1m;
+theta2 = theta2m - sym(pi)/2;
+theta3 = -1*theta2m + theta3m + sym(pi)/2;
+
+h01_update =  [[cos(theta1) 0 -sin(theta1) 0];
+    [sin(theta1) 0 cos(theta1) 0];
+    [0 -1 0 0.254];
+    [0 0 0 1]];
+
+h12_update = [[cos(theta2) -sin(theta2) 0 0.254*cos(theta2)];
+    [sin(theta2) cos(theta2) 0 0.254*sin(theta2)];
+    [0 0 1 0];
+    [0 0 0 1]];
+h23_update = [[cos(theta3) -sin(theta3) 0 0.254*cos(theta3)];
+    [sin(theta3) cos(theta3) 0 0.254*sin(theta3)];
+    [0 0 1 0];
+    [0 0 0 1]];
+
+h03_update = h01_update*h12_update*h23_update;
+
+h03_update = vpa(simplify(h03_update));
+
--- workspace/me446_lab_starter/me446lab.c
+++ workspace/me446_lab_starter/me446lab.c
@@ -7,8 +7,8 @@
 #define GRAV        9.81
 
 // These two offsets are only used in the main file user_CRSRobot.c  You just need to create them here and find the correct offset and then these offset will adjust the encoder readings
-float offset_Enc2_rad = -0.40;//0; //-0.37; pks11
-float offset_Enc3_rad = 0.228; //0.27; pks11
+float offset_Enc2_rad = -0.37;
+float offset_Enc3_rad = 0.27;
 
 
 // Your global varialbes.
@@ -20,11 +20,6 @@ float whattoprint = 0.0;
 
 #pragma DATA_SECTION(theta1array, ".my_arrs")
 float theta1array[100];
-
-#pragma DATA_SECTION(theta2array,".my_arrs") //pks11 - adding theta2
-float theta2array[100];
-
-float theta3 = 0;
 
 long arrayindex = 0;
 int UARTprint = 0;
@@ -39,16 +34,6 @@ float Simulink_PlotVar2 = 0;
 float Simulink_PlotVar3 = 0;
 float Simulink_PlotVar4 = 0;
 
-//pks11
-float  x_endeffector = 0;
-float  y_endeffector = 0;
-float  z_endeffector = 0;
-
-float theta1_calc = 0;
-float theta2_calc = 0;
-float theta3_calc = 0;
-
-float L = 0.254; //pks11 // Link parameter
 
 void mains_code(void);
 
@@ -67,7 +52,6 @@ void main(void)
 void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float *tau2,float *tau3, int error) {
 
 
-    //*tau1 = 0;
     *tau1 = 0;
     *tau2 = 0;
     *tau3 = 0;
@@ -78,7 +62,6 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
     if ((mycount%50)==0) {
 
         theta1array[arrayindex] = theta1motor;
-        theta2array[arrayindex] = theta2motor;
 
         if (arrayindex >= 99) {
             arrayindex = 0;
@@ -98,20 +81,6 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
     printtheta2motor = theta2motor;
     printtheta3motor = theta3motor;
 
-    //pks11 adding a variable
-    theta3 = theta3motor;
-
-    //pks11 adding a calculation of h03 with motor angles
-    x_endeffector = 0.254*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor));
-    y_endeffector = 0.254*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor));
-    z_endeffector = 0.254*cos(theta2motor) - 0.254*sin(theta3motor) + 0.254;
-
-    //pks11_ calculating thetas from inverese kinematics
-    theta1_calc = atan(y_endeffector/x_endeffector);
-    theta3_calc = acos((pow((L- z_endeffector),2) + pow(x_endeffector,2) + pow(y_endeffector,2) - 2*pow(L,2))/(2*pow(L,2)));
-    theta2_calc = acos(sqrt((pow(x_endeffector,2) + pow(y_endeffector,2))/((pow(x_endeffector,2) + pow(y_endeffector,2) + pow((L- z_endeffector),2))))) - (theta3_calc/2);
-
-
     Simulink_PlotVar1 = theta1motor;
     Simulink_PlotVar2 = theta2motor;
     Simulink_PlotVar3 = theta3motor;
@@ -122,12 +91,7 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
 
 void printing(void){
     if (whattoprint == 0) {
-        //serial_printf(&SerialA, "%.2f %.2f,%.2f   \n\r",printtheta1motor*180/PI,printtheta2motor*180/PI,printtheta3motor*180/PI);
-//  serial_printf(&SerialA, "%.2f %.2f,%.2f,%.2f   \n\r",printtheta1motor*180/PI,printtheta2motor*180/PI,printtheta3motor*180/PI,theta3); //printing theta3 in radians as well! pks11
         serial_printf(&SerialA, "%.2f %.2f,%.2f   \n\r",printtheta1motor*180/PI,printtheta2motor*180/PI,printtheta3motor*180/PI);
-        serial_printf(&SerialA, "%.2f %.2f,%.2f   \n\r",x_endeffector,y_endeffector,z_endeffector);
-        serial_printf(&SerialA, "%.2f %.2f,%.2f   \n\r",theta1_calc,theta2_calc,theta3_calc);
-
     } else {
         serial_printf(&SerialA, "Print test   \n\r");
     }
